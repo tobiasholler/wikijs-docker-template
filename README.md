6900e937d4ceb27bcd26357fda8d04700c4886bd
Tobias Holler <mail@toubs.de>
2020-07-28 17:13:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# wikijs-docker-template
+
+```sh
+git clone --depth=1 https://github.com/tobiasholler/wikijs-docker-template my_wikijs_name
+cd my_wikijs_name
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+randpw(){ < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;}
+
+read -p "Container Name: " containername
+password=$(randpw)
+
+sed -i "s/__CONTAINER__/${containername}/g" docker-compose.yml
+sed -i "s/__DB_PASSWORD_REPLACE_ME__/${password}/g" docker-compose.yml
+
+rm setup.sh
+rm -r .git
+
+echo "Created"
